781ad49fb3bfeed411d8f9d3eb7a408c0d57a2ea
何泽鹏 <275688470@qq.com>
2019-04-26 17:38:24 +0800

    统一网页标题、更改后台接口地址

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  title: '考勤管理系统',
+  title: '在线考勤管理系统',
 
   /**
    * @type {boolean} true | false
--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'vue-admin'
+const title = defaultSettings.title || '在线考勤管理系统'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
